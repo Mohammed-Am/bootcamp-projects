a5e238ed90cca9f630e7e3c6d4c0d1dfffd7a464
Mohammed-Am <mh.amouzoun@gmail.com>
2025-05-06 12:08:42 +0100

    Updated the project with new changes

---
+++ Week-1/day-1/challenge/challenge_Gold.py
@@ -0,0 +1,14 @@
+birthdate = input("Enter you birthday , DD/MM/YYYY")
+age_user =
+
+
+
+
+print(f"       ___{}___")
+print("      |:H:a:p:p:y:|")
+print("    __|___________|__")
+print("   |^^^^^^^^^^^^^^^^^|")
+print("   |:B:i:r:t:h:d:a:y:|")
+print("   |                 |")
+print("   ~~~~~~~~~~~~~~~~~~~")
+
