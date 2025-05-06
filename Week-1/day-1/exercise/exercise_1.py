5ca932ed211860875864b704a9449ace313d3552
Mohammed-Am <mh.amouzoun@gmail.com>
2025-05-06 09:53:13 +0100

    Updated the project with new changes

---
+++ Week-1/day-1/exercise/exercise_1.py
@@ -0,0 +1,67 @@
+## Exercice 1
+print("Hello world")
+print("Hello world")
+print("Hello world")
+print("Hello world")
+
+## Exercice 2
+print((99**3)**8)
+
+## Exercice 3
+my_name = "Mohammed"
+user_name = input("What's your name?")
+
+if my_name === user_name :
+    print('we have the same name😂')
+
+## Exercice 4
+height = input('type your height in centimeters')
+if height > 145 :
+    print("your tall enough")
+else
+     print( 'You need grow some more to ride ')   
+
+##5 Seters
+
+my_fav_numbers = set()
+my_fav_numbers = {14, 19 , 20}
+my_fav_numbers.add(20,25)
+my_fav_numbers.pop(4)
+friend_fav_number = {'0661716519','0678956315','0689547161'}
+our_fav_number = {my_fav_numbers + our_fav_number}
+
+##6  tuple
+
+ 
+my_tuple = (1, 2, 3)
+
+
+new_tuple = my_tuple + (4, 5)
+
+print(new_tuple)  #
+
+
+
+## 7 List 
+
+ basket = ["Banana", "Apples", "Oranges", "Blueberries"];
+ basket.remove('Banana')
+ basket.pop(4)
+ basket.append('Kiwi')
+ basket.insert(0 ,'Appels')
+
+
+ apple_count = basket.count("Apples")
+print(f"Number of Apples in the basket: {apple_count}")
+
+ print(basket)
+
+ ## 8  Orders
+
+ sandwich_orders = ["Tuna sandwich", "Pastrami sandwich", "Avocado sandwich", "Pastrami sandwich", "Egg sandwich", "Chicken sandwich", "Pastrami sandwich"].
+
+while "Pastrami sandwich" in sandwich_orders:
+    sandwich_orders.remove("Pastrami sandwich")
+
+finished_sandwiches = []
+while sandwich_orders:andwich_orders = ["Tuna sandwich", "Pastrami sandwich", "Avocado sandwich", "Pastrami sandwich", "Egg
