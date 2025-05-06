3f7b881999fcd8c0daa2d8c8a2b8256f913ac9e5
Mohammed-Am <mh.amouzoun@gmail.com>
2025-05-06 11:50:34 +0100

    Updated the project with new changes

---
+++ Week-1/day-1/challenge/challenge.py
@@ -0,0 +1,20 @@
+
+##1 : 
+
+number = int(input("Enter a number: "))
+length = int(input("Enter a length: "))
+
+list_of_numbers = [number * i for i in range(1, length + 1)]
+print(list_of_numbers)
+
+
+##2 :
+
+user_input = input("type somthing : ")
+
+print(''.join(set(user_input)))
+
+
+
+
+
