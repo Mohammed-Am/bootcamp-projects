2c71ccc13de561929cb9492205188e1f98c05b7c
Mohammed-Am <mh.amouzoun@gmail.com>
2025-05-08 10:28:19 +0100

    updat

---
+++ Week-1/day-3/exercise/exercises_gold.py
@@ -0,0 +1,17 @@
+class Circle:
+
+    def __init__(self, r:1.0):
+        self.r = r
+
+    def  perimeter(self):
+        return 2 * 3.14 * self.r
+
+    def  area(self):
+         return 3.14 * self.r **2    
+
+n = Circle(9)
+
+print(f"result of parimeter = {n.perimeter()}")
+print(f"result of area = {n.area()}")
+
+
