e4dc9f549b4ea707bae3b123765bc6289026cb20
Mohammed-Am <mh.amouzoun@gmail.com>
2025-05-08 11:59:47 +0100

    d3

---
+++ Week-1/day-3/exercise/practiceNinja.py
@@ -0,0 +1,20 @@
+class Phone:
+    def __init__(self,phone_number,message=[]):
+        self.phone_number = phone_number
+
+     call_history = []
+
+
+     def call(self,  other_phone):
+        print("0661713519")
+
+     def show_call_history(self , call_history):
+        print(self.call_history)
+
+     def send_message(self, to , from , content):
+
+
+show_outgoing_messages(self), show_incoming_messages(self), show_messages_from(self)
+
+
+
