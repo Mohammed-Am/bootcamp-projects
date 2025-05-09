2db5c2e54df193c675470f804a2b62aac1ab307b
Mohammed-Am <mh.amouzoun@gmail.com>
2025-05-09 13:23:12 +0100

    Initial

--- Week-1/day-5/wer9a_mi9as/game.py
+++ Week-1/day-5/wer9a_mi9as/game.py
@@ -1,2 +1,20 @@
+import random
+
 class Game:
-    get_user_item(self):
+     get_user_item(self,  user_item):
+        self.item = input("Select (r)ock, (p)aper,or (s)cissors: ")
+        return self.item
+
+     get_computer_item(self):
+       computer_item = ["r","p","s"]
+        return random.choice(computer_item)
+
+     get_game_result(self, user_item, computer_item):
+        self.user_item = user_item
+        self.computer_item = computer_item
+
+        if user_item == computer_item:
+            print (f"You chose: {user_item}. The computer chose: {computer_item}. Result: {}")
+
+
+          
