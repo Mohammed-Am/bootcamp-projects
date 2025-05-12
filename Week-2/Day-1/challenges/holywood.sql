0df4e65f2b2df3b80ed9653b969f96646d75c420
Mohammed-Am <mh.amouzoun@gmail.com>
2025-05-12 17:17:00 +0100

    commit message

---
+++ Week-2/Day-1/challenges/holywood.sql
@@ -0,0 +1,6 @@
+Select COUNT(*) from actors;
+
+
+INSERT INTO actors (first_name,birthdate,number_oscars) values ('L9AYED','1984-9-9','8')
+
+--the last name i give it  not-null type when i insert a donne first name , that's mean i should insert somthing to the column
