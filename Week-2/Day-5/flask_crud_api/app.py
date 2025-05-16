3464deb19a5edfcbf6b92d3f0afcedb210a3e0d6
Mohammed-Am <mh.amouzoun@gmail.com>
2025-05-16 20:33:05 +0100

    Initial commit

---
+++ Week-2/Day-5/flask_crud_api/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+from config import Config
+from models import User, db
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db.init_app(app)
+
+
+@app.route('/users')
+def users():
+    return jsonify({'name': 'Testssssssss user'})
+
+
+
+if __name__ == '__main__':
+    with app.app_context():
+         db.create_all()
+    app.run(debug = True, port = 5001)
---
+++ Week-2/Day-5/flask_crud_api/config.py
@@ -0,0 +1,2 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI='postgresql://postgres:kizon@localhost:5432/flask_crud_db'
