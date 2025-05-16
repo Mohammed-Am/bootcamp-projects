b5d59c015a2eebb70af5b06507cb01e745f308b1
Mohammed-Am <mh.amouzoun@gmail.com>
2025-05-16 23:23:21 +0100

    Initial commit

--- Week-2/Day-5/flask_crud_api/app.py
+++ Week-2/Day-5/flask_crud_api/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify , request
 from config import Config
 from models import User, db
 
@@ -9,11 +9,21 @@ db.init_app(app)
 
 @app.route('/users')
 def users():
-    return jsonify({'name': 'Testssssssss user'})
+    users = User.query.all()
+    return jsonify([user.to_dict() for user in users])
+
+@app.route('/users',methods=['POST'])
+def create_users():
+    data = request.get_json()
+    new_user = User(username=data['username'],email=data['email'])
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify(new_user.to_dict()), 201
+
 
 
 
 if __name__ == '__main__':
     with app.app_context():
          db.create_all()
-    app.run(debug = True, port = 5001)
+    app.run(debug = True, port = 5000)
