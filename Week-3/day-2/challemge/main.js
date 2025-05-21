09cf3b5ba709f46119c9df316fbf8dcd03d344be
Mohammed-Am <mh.amouzoun@gmail.com>
2025-05-21 09:46:26 +0100

    Initial commit

--- Week-3/day-2/challemge/main.js
+++ Week-3/day-2/challemge/main.js
@@ -1,18 +1,27 @@
-let sentence = "The movie is not that bad, I like it"
+let sentence = "This dinner is not that bad ! You cook well"
     console.log(sentence)
 
     
     const word = "not";
-    const index = sentence.indexOf(word);
-    const newSentence = sentence.substring(index);
+    const indexNot = sentence.indexOf(word);
+    const newSentence = sentence.substring(indexNot);
     console.log(newSentence); 
 
 
     const word2 = "bad";
-    const index2 = sentence.indexOf(word2);
-    const newSentence2 = sentence.substring(index2);
+    const indexBad = sentence.indexOf(word2);
+    const newSentence2 = sentence.substring(indexBad);
     console.log(newSentence2); 
 
+
+    if(indexBad > indexNot ){
+        result = sentence.replace('not that bad' , 'good')
+        console.log(result)
+    }
+
+    if(indexBad > indexNot || word2 != sentence){
+        console.log(sentence)
+    }
     
 
  
