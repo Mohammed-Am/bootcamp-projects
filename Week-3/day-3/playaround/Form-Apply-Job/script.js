364e67b612583e2141030803bc76e40ee9358908
Mohammed-Am <mh.amouzoun@gmail.com>
2025-05-22 07:39:53 +0100

    Initial commit

---
+++ Week-3/day-3/playaround/Form-Apply-Job/script.js
@@ -0,0 +1,19 @@
+const form = document.querySelector('form')
+document.getElementById('preview').style.display  = "none";
+
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault();
+
+    const formData = new FormData(form);
+
+    for(item of formData) {
+        console.log(item[0], item[1])
+    }
+
+
+
+
+document.getElementById('preview').style.display  = "";
+})
+
