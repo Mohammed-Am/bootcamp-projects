77d9d6072dae188d6e771b36a76f930b95ee1a30
Mohammed-Am <mh.amouzoun@gmail.com>
2025-05-27 23:18:45 +0100

    Initial commit

---
+++ Week-4/day-2/Challenges/carInventory.js
@@ -0,0 +1,29 @@
+const inventory = [
+  { id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
+  { id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
+  { id: 3, car_make: "Honda", car_model: "Accord", car_year: 1983 },
+  { id: 4, car_make: "Land Rover", car_model: "Defender Ice Edition", car_year: 2010 },
+  { id: 5, car_make: "Honda", car_model: "Accord", car_year: 1995 },
+];
+
+
+function getCarHonda(carInventory) {
+     let car =  carInventory.find((car) => car.car_make === 'Honda');
+       return   `This is a ${car.car_make} ${car.car_model} from ${car.car_year}.`
+
+}
+
+console.log(getCarHonda(inventory));
+
+
+//part2
+function sortCarInventoryByYear(carInventory) {
+  let sortedInventory = [...carInventory].sort((a, b) => a.car_year - b.car_year);
+  let result = [];
+  sortedInventory.forEach((car) => {
+    result.push(`This is a ${car.car_make} ${car.car_model} from ${car.car_year}.`);
+  });
+  return result;
+}
+
+console.log(sortCarInventoryByYear(inventory));
