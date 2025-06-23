6567501f2c953719349c263f390811355a354603
Mohammed-Am <mh.amouzoun@gmail.com>
2025-06-23 19:15:28 +0100

    Initial commit

--- Week-7/day-4/Challenge/server/server.js
+++ Week-7/day-4/Challenge/server/server.js
@@ -2,20 +2,17 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-// Middleware to parse JSON and enable CORS
 app.use(cors());
 app.use(express.json());
 
-// GET request for /api/hello
 app.get('/api/hello', (req, res) => {
-  res.send('Hello From Express');
+  res.send('hi');
 });
 
-// POST request for /api/world
 app.post('/api/world', (req, res) => {
   const { value } = req.body;
-  console.log('Request body:', req.body); // Log the request body
-  res.send(`I received your POST request. This is what you sent me: ${value}`);
+  console.log('Request body:', req.body);
+  res.send(`this is what you sent to me: ${value}`);
 });
 
 app.listen(5000, () => console.log('Server running on port 5000'));
